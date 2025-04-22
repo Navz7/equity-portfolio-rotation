7290032246bebe678b6ff01bf958e8d9eb405689
root <root@LAPTOP-TB4ID2AL.localdomain>
2025-04-22 17:15:26 +0530

    Introducing Vader Analysis to take care of conflicting news

--- src/sentiment_analyzer.py
+++ src/sentiment_analyzer.py
@@ -2,6 +2,7 @@ import json
 import os
 import pandas as pd
 from transformers import BertTokenizer, BertForSequenceClassification, pipeline
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
 from tqdm import tqdm
 
@@ -67,16 +68,41 @@ def apply_finbert_sentiment(df, text_col='text'):
         try:
             result = finbert(text[:512])  # BERT limit
             return pd.Series({
-                "sentiment": result[0]['label'],
-                "confidence": result[0]['score']
+                "finbert_sentiment": result[0]['label'],
+                "finbert_score": result[0]['score']
             })
         except Exception as e:
-            return pd.Series({"sentiment": "ERROR", "confidence": 0.0})
+            return pd.Series({"finbert_sentiment": "ERROR", "finbert_score": 0.0})
 
     # Apply sentiment to DataFrame
     sentiment_df = df[text_col].progress_apply(get_sentiment)
     return pd.concat([df, sentiment_df], axis=1)
 
+def apply_vader_sentiment(df, text_col='text'):
+    tqdm.pandas()
+    analyzer = SentimentIntensityAnalyzer()
+
+    def get_vader_sentiment(text):
+        try:
+            score = analyzer.polarity_scores(text)['compound']
+            if score >= 0.05:
+                sentiment = 'positive'
+            elif score <= -0.05:
+                sentiment = 'negative'
+            else:
+                sentiment = 'neutral'
+            return pd.Series({
+                "vader_sentiment": sentiment,
+                "vader_score": score
+            })
+        except Exception as e:
+            return pd.Series({
+                "vader_sentiment": "ERROR",
+                "vader_score": 0.0
+            })
+
+    vader_df = df[text_col].progress_apply(get_vader_sentiment)
+    return pd.concat([df, vader_df], axis=1)
 
 # Load model just once
 finbert_model_name = "ProsusAI/finbert"
@@ -89,6 +115,9 @@ all_articles = load_raw_data(raw_data_dir)
 
 df = preprocess_articles(all_articles)
 
-#Applying Sentiment analysis to existing df--> sentiment + confidence will be tracked.
+#Applying finbert Sentiment analysis to existing df--> sentiment + confidence will be tracked.
 df = apply_finbert_sentiment(df, text_col='text')
-print(df.head())
+# Incorporating vader sentiment analysis as a secondary check -> There may be chances of conflicting info from diff news sources.
+df = apply_vader_sentiment(df, text_col='text')
+out_path = "/mnt/c/Users/LENOVO/Documents/EquityPortfolioRotation/equity-portfolio-rotation/data/sentiment_results/sentiment_results.csv"
+df.to_csv(out_path, index=False)
